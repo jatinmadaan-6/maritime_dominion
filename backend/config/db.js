983c45169002eaa5c9405f0f096ab3a7c150f9f7
jatinmadaan-6 <jatinalchemy@gmail.com>
2026-07-25 18:34:42 +0530

    Update database connection configuration variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 4000,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.HOST,
+  port: process.env.PORT || 4000,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   ssl: {
