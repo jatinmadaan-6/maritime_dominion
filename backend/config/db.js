35c52e7641fede9e0ea2046ec538e22d37d00ff0
Jatin Madaan <jatinalchemy@gmail.com>
2026-07-28 22:10:59 +0530

    added fallbacks values to db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,16 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-<<<<<<< HEAD
-  host: process.env.HOST,
+  host: process.env.HOST || "localhost",
   port: process.env.PORT || 4000,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  user: process.env.USERNAME || "root",
+  password: process.env.PASSWORD || "",
+  database: process.env.DATABASE || "maritime",
   waitForConnections: true,
   connectionLimit: 10,
   ssl: {
     minVersion: "TLSv1.2"
   }
-=======
-  host: process.env.DB_HOST || "localhost", user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "", database: process.env.DB_NAME || "maritime",
-  waitForConnections: true, connectionLimit: 10, queueLimit: 0,
->>>>>>> master
 });
 
 module.exports = pool;
